772afbfaa1896cd0c64e824248268aa7636554c1
Shane Main <mainsha@protonmail.com>
2024-09-09 21:57:16 -0400

    directory refactor

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
-from src.Qt.primary_window import Mario64All
-from src.logic.check_dependencies import check_and_install
+from src.core.check_dependencies import check_and_install
+from src.ui.primary_window import Mario64All
 
 if __name__ == "__main__":
     check_and_install()
