619beb1700b76ccfb5de63ba33f04d3cef4690d8
Shane Main <mainsha@protonmail.com>
2024-09-10 21:12:52 -0400

    dependency checker rework

--- src/main.py
+++ src/main.py
@@ -2,11 +2,9 @@ import sys
 
 from PyQt6.QtWidgets import QApplication
 
-from src.core.check_dependencies import check_and_install
 from src.ui.primary_window import Mario64All
 
 if __name__ == "__main__":
-    check_and_install()
     app = QApplication(sys.argv)
     window = Mario64All()
     window.show()
